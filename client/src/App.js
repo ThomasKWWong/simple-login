6079a2655b7f3b19ae48444d1230beaa69bbc1ff
Thomas Wong <thomaskwwong7@gmail.com>
2023-08-26 10:42:53 -0400

    added 404 page

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Login from './pages/Login';
 import Register from './pages/Register'; 
+import NoPage from './pages/NoPage';
 import React from 'react';
 
 const App = () => {
@@ -11,6 +12,7 @@ const App = () => {
                     <Route index element = {<Login />} />
                     <Route path='/login' element = {<Login />} />
                     <Route path='/register' element = {<Register />} />
+                    <Route path="*" element = {<NoPage />} />
                 </Routes>
             </BrowserRouter>
         </div>
