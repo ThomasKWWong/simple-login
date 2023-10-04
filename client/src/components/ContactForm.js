59f5ddd9a9bf0b776dfc9bbd31de9d8c7d34f7f1
Thomas Wong <thomaskwwong7@gmail.com>
2023-10-04 02:28:13 -0400

    added contact page functionality

--- client/src/components/ContactForm.js
+++ client/src/components/ContactForm.js
@@ -1,17 +1,32 @@
-import React from 'react';
+import React, {useRef} from 'react';
 import { SForm, SInput, SButton, SFormControl, SLabel, SFormTitle, SDiv, SDivL } from './styles';
+import emailjs from '@emailjs/browser';
 
 const ContactForm = () => {
+    const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_wvn76eh', 'template_1md0jk5', form.current, 'dsnlYoiE9sspgE6Bk')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+  };
+
+
     return(
         <SDiv>
             <SDivL>
-            <SForm>
+            <SForm ref={form} onSubmit={sendEmail}>
                 <SFormTitle>Contact Form</SFormTitle>
                 <SFormControl>
                     <SLabel>Email</SLabel>
-                    <SInput type="text" placeholder='Type your email'></SInput>
+                    <SInput type="text" placeholder='Type your email' name="user_email"></SInput>
                     <SLabel>Subject</SLabel>
-                    <SInput type="text" placeholder='Type your message subject'></SInput>
+                    <SInput type="text" placeholder='Type your message subject' name="message"></SInput>
                 </SFormControl>
                 <SButton type = "submit">Submit</SButton>
             </SForm>
